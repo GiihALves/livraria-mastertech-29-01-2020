0211aa9ca6239675e2cf02daedd327c8b1bbae0c
Gislaine Alves <gislaaineealves@gmail.com>
2020-01-29 12:12:37 -0300

    Livraria mastertech correção em sala

---
+++ src/main/java/br/com/groger/livraria/controller/livrariaController.java
@@ -0,0 +1,38 @@
+package br.com.groger.livraria.controller;
+
+import br.com.groger.livraria.model.livrariaModel;
+import br.com.groger.livraria.service.livrariaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class livrariaController {
+
+    @Autowired
+    private livrariaService service;
+
+    @GetMapping
+    public String mostrarHome(){
+        return "index";
+    }
+
+    @PostMapping("/cadastrar")
+    public String cadastrarLivro(livrariaModel Livros){
+        service.cadastrarLivro(Livros);
+        return "index";
+    }
+
+    @GetMapping("/livros")
+    public ModelAndView listarLivros(){
+        ModelAndView pagina = new ModelAndView("listarLivros");
+        Iterable<livrariaModel> livros = service.listarLivros();
+        pagina.addObject("livros", livros);
+        return pagina;
+
+
+    }
+
+}
